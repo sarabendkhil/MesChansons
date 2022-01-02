23030262e1100166b93fb044aa6304811a06c7b4
sarabendkhil <sarrabendkhil5@gmail.com>
2022-01-02 20:10:40 +0100

    first commit

--- src/app/add-chanson/add-chanson.component.ts
+++ src/app/add-chanson/add-chanson.component.ts
@@ -1,3 +1,4 @@
+import { Manager } from './../model/manager';
 import { Component, OnInit } from '@angular/core';
 import { Chanson } from '../model/chanson.model';
 import { ChansonService } from '../services/chanson.service';
@@ -11,13 +12,19 @@ export class AddChansonComponent implements OnInit {
 
   newChanson = new Chanson();
   message:string;
+  managers : Manager[];
+  newIdMan : number;
+  newManager : Manager;
   
   constructor(private chansonService : ChansonService,private router:Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.managers = this.chansonService.listeManagers();  
+  }
   addChanson(){
+    this.newManager = this.chansonService.consulterManager(this.newIdMan);
+    this.newChanson.manager=this.newManager;
     this.chansonService.ajouterChanson(this.newChanson);
-    //this.message="Chanson "+this.newChanson.nomChanson+" ajouté avec succés!";
     this.router.navigate(["chansons"]);
     }
 
--- src/app/services/chanson.service.ts
+++ src/app/services/chanson.service.ts
@@ -1,5 +1,6 @@
+import { Chanson } from './../model/chanson.model';
+import { Manager } from './../model/manager';
 import { Injectable } from '@angular/core';
-import { Chanson } from '../model/chanson.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +8,18 @@ import { Chanson } from '../model/chanson.model';
 export class ChansonService {
 
   chansons :Chanson[];
-  
+  managers:Manager[];
+
+  manager=new Manager();
 
   constructor() {
+    this.managers = [ {idMan : 1, nomMan : "Jonathan Dickins"},
+                      {idMan : 2, nomMan : "Jim Beach"},
+                      {idMan : 3, nomMan : "Stuart Camp"}];
     this.chansons = [
-      {idChanson : 1, nomChanson : "Someone like you", nomChanteur : "Adel", datePublication : new Date("02/15/2011")},
-      {idChanson : 2, nomChanson : "Bohemian Rhapsody", nomChanteur : "Queen", datePublication : new Date("10/31/1975")},
-      {idChanson : 3, nomChanson :"Perfect", nomChanteur : "Ed Sheeran", datePublication : new Date("11/24/2017")}
+      {idChanson : 1, nomChanson : "Someone like you", nomChanteur : "Adele", datePublication : new Date("02/15/2011"), manager:{idMan:1,nomMan:"Jonathan Dickins"}},
+      {idChanson : 2, nomChanson : "Bohemian Rhapsody", nomChanteur : "Queen", datePublication : new Date("10/31/1975"), manager:{idMan:2,nomMan:"Jim Beach"}},
+      {idChanson : 3, nomChanson :"Perfect", nomChanteur : "Ed Sheeran", datePublication : new Date("11/24/2017"), manager:{idMan:3,nomMan:"Stuart Camp"}}
       ];
    }
 
@@ -53,4 +59,12 @@ export class ChansonService {
       this.ajouterChanson(c);
       this.trierChansons();
     }
+    listeManagers():Manager[] {
+      return this.managers;
+      }
+      
+      consulterManager(id:number): Manager{
+      this.manager = this.managers.find(man => man.idMan == id);
+      return this.manager;
+      }
 }
